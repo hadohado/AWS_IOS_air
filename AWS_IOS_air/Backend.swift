a0175451695da7a64c48bf20cb8d11052c43a04a
hadohado <hadohado@gmail.com>
2021-05-13 15:08:44 -0700

    mod 3  configure amplify authentication lib at runtime

---
+++ AWS_IOS_air/Backend.swift
@@ -0,0 +1,66 @@
+//
+//  Backend.swift
+//  AWS_IOS_air
+//
+//  Created by ha tuong do on 5/13/21.
+//  Copyright © 2021 alpine. All rights reserved.
+//
+import AmplifyPlugins
+
+import Foundation
+import UIKit
+import Amplify
+
+class Backend {
+    static let shared = Backend()
+    static func initialize() -> Backend {
+        return .shared
+    }
+    private init() {
+      // initialize amplify
+      do {
+        try Amplify.add(plugin: AWSCognitoAuthPlugin())
+        
+        try Amplify.configure()
+        print("Initialized Amplify");
+      } catch {
+        print("Could not initialize Amplify: \(error)")
+      }
+    }
+    // MARK: - User Authentication
+
+    // signin with Cognito web user interface
+    public func signIn() {
+
+        _ = Amplify.Auth.signInWithWebUI(presentationAnchor: UIApplication.shared.windows.first!) { result in
+            switch result {
+            case .success(_):
+                print("Sign in succeeded")
+            case .failure(let error):
+                print("Sign in failed \(error)")
+            }
+        }
+    }
+
+    // signout
+    public func signOut() {
+
+        _ = Amplify.Auth.signOut() { (result) in
+            switch result {
+            case .success:
+                print("Successfully signed out")
+            case .failure(let error):
+                print("Sign out failed with error \(error)")
+            }
+        }
+    }
+
+    // change our internal state, this triggers an UI update on the main thread
+    func updateUserData(withSignInStatus status : Bool) {
+        DispatchQueue.main.async() {
+            let userData : UserData = .shared
+            userData.isSignedIn = status
+        }
+    }
+}
+
