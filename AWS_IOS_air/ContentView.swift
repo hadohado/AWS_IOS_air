e5f1dd1e39c1c527c3f68f4419ecb5539f5f8ed4
hadohado <hadohado@gmail.com>
2021-05-13 12:54:30 -0700

    Initial Commit

---
+++ AWS_IOS_air/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AWS_IOS_air
+//
+//  Created by ha tuong do on 5/13/21.
+//  Copyright © 2021 alpine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
